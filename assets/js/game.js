83ae2f76843ac0b03cad29f877410d6eb8431011
lucasr0609 <lucasram2583@gmail.com>
2022-06-26 16:48:42 -0400

    created/execute functions, interact with the player and stored data.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
 window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+// What is this? logs consoles of the running code
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this? adding a sentance with "our robot's name is" + tells it to find the varible name playerName
+console.log("Our robot's name is " + playerName);
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+function fight() {
+    window.alert("This fight has begun!");
+}
+// fight();
