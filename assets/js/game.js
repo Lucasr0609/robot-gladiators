0ff38384a479f4407d22beb3d8f6f6e59c6871f9
lucasr0609 <lucasram2583@gmail.com>
2022-08-09 02:04:48 -0400

    Fixed Fight and Skip Prompt and also allowed case letters

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,35 +8,45 @@ var randomNumber = function(min, max) {
 };
 
 var fightOrSkip = function() {
-  // ask player if they'd like to fight or skip using fightOrSkip function
+  // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
-  // Enter the conditional recursive function call here!
-  // Conditional Recursive Function Call
+  // validate prompt answer
   if (promptFight === "" || promptFight === null) {
-    alert("You need to provide a valid answer! Please try again.");
+    window.alert("You need to provide a valid answer! Please try again.");
+    // use return to call it again and stop the rest of this function from running
     return fightOrSkip();
   }
 
-  // if player picks "skip" confirm and then stop the loop
-  if (promptFight === "skip" || promptFight === "SKIP") {
+  // convert promptFight to all lowercase so we can check with less options
+  promptFight = promptFight.toLowerCase();
+
+  if (promptFight === "skip") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
     // if yes (true), leave fight
     if (confirmSkip) {
       window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerInfo.playerMoney = playerInfo.money - 10;
-      shop();
+      // subtract money from playerMoney for skipping, but don't let them go into the negative
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+      // stop while() loop using break; and enter next fight
+
+      // return true if player wants to leave
+      return true;
     }
   }
-}
+  return false;
+};
 
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
-    fightOrSkip(); // <-- Replace code with this function call
+   // ask player if they'd like to fight or skip using fightOrSkip function
+   if (fightOrSkip()) {
+    // if true, leave fight by breaking loop
+    break;
+  }
     // generate random damage value based on player's attack power
     var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
